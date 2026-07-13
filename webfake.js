2b558088cdaa047324cf6fd5cb356dc8484feeb0
my-websitesolution <satriorich2011@gmail.com>
2026-07-13 22:03:13 +0700

    Update webfake.js

--- webfake.js
+++ webfake.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".navbar a"); // Ambil semua link di dalam menu
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
+});
+
+// 2. Fungsi untuk menutup menu (menghapus class 'active')
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active"); // Menghapus class active di tombol hamburger
+    navbar.classList.remove("active"); // Menghapus class active di daftar menu agar menutup
+  });
 });
